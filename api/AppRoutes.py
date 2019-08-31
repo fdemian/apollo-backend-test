4bb52924f28a39d72b477aad133e08c0779582e1
fdemian <demian.fc@gmail.com>
2019-08-31 15:24:31 -0300

    Correctly save file.

--- api/AppRoutes.py
+++ api/AppRoutes.py
@@ -2,7 +2,7 @@
 #from api import GraphqlServer
 from tornado.web import StaticFileHandler
 import cgi
-
+from os import getcwd, path
 import json
 from tornado.web import RequestHandler
 from ariadne import QueryType, MutationType, graphql_sync, make_executable_schema, upload_scalar, combine_multipart_data
@@ -11,7 +11,9 @@ type_defs = """
       scalar Upload
       
       type UploadImagePayload {
-        status: Boolean!      
+        status: Boolean!,
+        path: String!,
+        mime_type: String!
       }
       
       type Query {
@@ -26,6 +28,7 @@ type_defs = """
 query = QueryType()
 mutations = MutationType()
 
+
 @query.field("hello")
 def resolve_hello(_, info):
     request = info.context
@@ -33,33 +36,29 @@ def resolve_hello(_, info):
     print(user_agent)
     return "Hello,Dude!"
 
+
 @mutations.field("uploadUserImage")
 @upload_scalar.serializer
 def resolve_upload(_, info, file):
-    print("-----")
     file_name = file[0]['filename']
     file_type = file[0]['content_type']
     file_content = file[0]['body']
-    print("-----------")
 
-    """
-    def download_avatar(url, username):
-        data = yield fetch_coroutine(url)
+    current_dir = getcwd()
+    file_path = path.join(current_dir, "static/") + file_name
 
-        current_dir = getcwd()
-        output_file_name = path.join(current_dir, "static/avatars/") + username + ".jpg"
-        save_file(output_file_name, data)
+    with open(file_path, "bw") as f:
+        f.write(file_content)
 
-        return username + ".jpg"
+    return {
+     "status": True,
+     "path": "static/" + file_name,
+     "mime_type": file_type
+    }
 
-    #Save file
-     with open(path, "bw") as f:
-          f.write(data)
-    """
-
-    return { "status": True }
 
 schema = make_executable_schema(type_defs, [query, mutations, upload_scalar])
+
 
 class GraphqlServer(RequestHandler):
 
@@ -69,22 +68,20 @@ class GraphqlServer(RequestHandler):
         content_type = self.request.headers['Content-Type']
 
         if "multipart/form-data" in content_type:
-           try:
-               operations = json.loads(self.get_body_argument("operations", default=None, strip=False))
-               map = json.loads(self.get_body_argument("map", default=None, strip=False))
-               data = combine_multipart_data(operations, map, self.request.files)
+            try:
+                operations = json.loads(self.get_body_argument("operations", default=None, strip=False))
+                map = json.loads(self.get_body_argument("map", default=None, strip=False))
+                data = combine_multipart_data(operations, map, self.request.files)
 
-           except Exception as ex:
-               print(ex)
-               print("ERROR!")
-               print("::::::::::::::::::::")
-               self.set_status(400, "Error")
-               self.set_header("Access-Control-Allow-Origin", "*")
-               self.set_header("Content-Type", "application/jsonp;charset=UTF-8")
-               return
+            except:
+                self.set_status(400, "Error")
+                self.set_header("Access-Control-Allow-Origin", "*")
+                self.set_header("Content-Type", "application/jsonp;charset=UTF-8")
+
+                return
 
         else:
-           data = json.loads(self.request.body.decode("utf-8"))
+            data = json.loads(self.request.body.decode("utf-8"))
 
         # Note: Passing the request to the context is optional.
         success, result = graphql_sync(schema, data, context_value=self.request, debug=True)
@@ -100,6 +97,7 @@ class GraphqlServer(RequestHandler):
           self.write(result)
 
         return
+
 
 def get_app_routes(static_path, notifications_enabled):
     routes = [
