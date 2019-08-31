4a88d6fe4acf3c2de1726888cd3647b7053bc2f4
fdemian <demian.fc@gmail.com>
2019-08-31 01:59:57 -0300

    Correctly handle file uploads.

--- api/AppRoutes.py
+++ api/AppRoutes.py
@@ -1,30 +1,110 @@
 # Route imports
-#from graphene_tornado.schema import schema
-from graphene_tornado.tornado_graphql_handler import TornadoGraphQLHandler
+#from api import GraphqlServer
 from tornado.web import StaticFileHandler
+import cgi
 
-from graphene import ObjectType, String, Schema
+import json
+from tornado.web import RequestHandler
+from ariadne import QueryType, MutationType, graphql_sync, make_executable_schema, upload_scalar, combine_multipart_data
 
-class Query(ObjectType):
-    # this defines a Field `hello` in our Schema with a single Argument `name`
-    hello = String(name=String(default_value="stranger"))
-    goodbye = String()
+type_defs = """      
+      scalar Upload
+      
+      type UploadImagePayload {
+        status: Boolean!      
+      }
+      
+      type Query {
+        hello: String!
+      }
+      
+      type Mutation {        
+        uploadUserImage(file: Upload!): UploadImagePayload                
+      }
+  """
 
-    # our Resolver method takes the GraphQL context (root, info) as well as
-    # Argument (name) for the Field and returns data for the query Response
-    def resolve_hello(root, info, name):
-        return f'Hello {name}!'
+query = QueryType()
+mutations = MutationType()
 
-    def resolve_goodbye(root, info):
-        return 'See ya!'
+@query.field("hello")
+def resolve_hello(_, info):
+    request = info.context
+    user_agent = request.headers.get("User-Agent", "Guest")
+    print(user_agent)
+    return "Hello,Dude!"
 
-schema = Schema(query=Query)
+@mutations.field("uploadUserImage")
+@upload_scalar.serializer
+def resolve_upload(_, info, file):
+    print("-----")
+    file_name = file[0]['filename']
+    file_type = file[0]['content_type']
+    file_content = file[0]['body']
+    print("-----------")
+
+    """
+    def download_avatar(url, username):
+        data = yield fetch_coroutine(url)
+
+        current_dir = getcwd()
+        output_file_name = path.join(current_dir, "static/avatars/") + username + ".jpg"
+        save_file(output_file_name, data)
+
+        return username + ".jpg"
+
+    #Save file
+     with open(path, "bw") as f:
+          f.write(data)
+    """
+
+    return { "status": True }
+
+schema = make_executable_schema(type_defs, [query, mutations, upload_scalar])
+
+class GraphqlServer(RequestHandler):
+
+    # GraphQL queries are always sent as POST
+    def post(self):
+
+        content_type = self.request.headers['Content-Type']
+
+        if "multipart/form-data" in content_type:
+           try:
+               operations = json.loads(self.get_body_argument("operations", default=None, strip=False))
+               map = json.loads(self.get_body_argument("map", default=None, strip=False))
+               data = combine_multipart_data(operations, map, self.request.files)
+
+           except Exception as ex:
+               print(ex)
+               print("ERROR!")
+               print("::::::::::::::::::::")
+               self.set_status(400, "Error")
+               self.set_header("Access-Control-Allow-Origin", "*")
+               self.set_header("Content-Type", "application/jsonp;charset=UTF-8")
+               return
+
+        else:
+           data = json.loads(self.request.body.decode("utf-8"))
+
+        # Note: Passing the request to the context is optional.
+        success, result = graphql_sync(schema, data, context_value=self.request, debug=True)
+
+        status_code = 200 if success else 400
+        status_text = 'Ok' if success else "Error"
+
+        self.set_status(status_code, status_text)
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Content-Type", "application/jsonp;charset=UTF-8")
+
+        if success:
+          self.write(result)
+
+        return
 
 def get_app_routes(static_path, notifications_enabled):
     routes = [
-       (r'/graphql', TornadoGraphQLHandler, dict(graphiql=True, schema=schema)),
-       (r'/graphql/batch', TornadoGraphQLHandler, dict(graphiql=True, schema=schema, batch=True)),
-       (r'/graphql/graphiql', TornadoGraphQLHandler, dict(graphiql=True, schema=schema)),
+       (r'/graphql', GraphqlServer),
+       (r'/graphql/(.*)', GraphqlServer),
        (r"/static/(.*)", StaticFileHandler, {"path": static_path}),
        (r"/(manifest\.json)", StaticFileHandler, {"path": static_path}),
        (r"/(favicon\.ico)", StaticFileHandler, {"path": static_path}),
